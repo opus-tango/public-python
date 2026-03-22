6f3d6cb4ec6abee45ce3d89722886833fb0dd353
Nayan Sawyer <33187059+opus-tango@users.noreply.github.com>
2026-03-22 19:13:31 -0400

    add script for terrafirmagreg bloomery optimization

---
+++ terrafirmagreg_bloomery.py
@@ -0,0 +1,12 @@
+num_ore = 19
+mb_per_ore = 129
+mb_per_ingot = 144
+mb_per_coal = 144
+
+mb = num_ore * mb_per_ore
+
+num_ingots = int(mb / mb_per_ingot)
+num_coal = int(mb / mb_per_coal)
+lost_mb = mb % mb_per_ingot
+
+print(f"With {num_ore} ore and {num_coal} coal, you will get {num_ingots} ingots and lose {lost_mb} MB of iron.")
